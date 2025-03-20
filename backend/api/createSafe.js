61a73972b6e8613365b4e364e3e4432bfcc32657
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-03-20 17:25:29 -0300

    get private key test user check if exists

--- backend/api/createSafe.js
+++ backend/api/createSafe.js
@@ -4,3 +4,8 @@ const Safe = require('@safe-global/protocol-kit').default
 // taken from documentation
 const { arbitrum } = require('viem/chains')
 const { Wallet } = require('ethers')
+
+const FIRST_OWNER_PK = process.env.PRIVATE_KEY
+if (!FIRST_OWNER_PK) {
+  throw new Error('First owner private key not set in .env (PRIVATE_KEY)')
+}
