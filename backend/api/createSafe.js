4278513436d21801061f7e302513a30238295a6e
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-03-20 17:24:27 -0300

    add comment

--- backend/api/createSafe.js
+++ backend/api/createSafe.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const { ethers } = require('ethers')
 const Safe = require('@safe-global/protocol-kit').default
+// taken from documentation
 const { arbitrum } = require('viem/chains')
 const { Wallet } = require('ethers')
