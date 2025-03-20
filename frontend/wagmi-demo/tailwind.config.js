93d32d17d8b633641f2ecc05933b519855b2ddfd
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-03-21 02:25:52 +0700

    🧹 cleanup: move merge styles

--- frontend/wagmi-demo/tailwind.config.js
+++ frontend/wagmi-demo/tailwind.config.js
@@ -1,12 +1,44 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    './chat/**/*.{js,jsx}',
     './app/**/*.{js,ts,jsx,tsx}',
+    './chat/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    fontFamily: {
+      'sans': ['Open Sans', 'sans-serif'],
+      'urbanist': ['Urbanist', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'primary-blue': 'rgb(146, 179, 202)',
+        'primary-orange': 'rgb(243, 195, 177)',
+        'main-text': 'rgb(0, 43, 49)',
+        'error-red': 'rgb(208, 69, 82)',
+      },
+      animation: {
+        'spinner': 'spinner 1.5s linear infinite',
+        'spinner-delayed': 'spinner 1.5s linear infinite 0.75s',
+      },
+      keyframes: {
+        spinner: {
+          '0%': { transform: 'scale(0)', opacity: 1 },
+          '100%': { transform: 'scale(1)', opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
