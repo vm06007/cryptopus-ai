0f9f04e254ed7fa3d33c71a1dfec033c5fe0f0c5
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-03-20 17:27:21 -0300

    create new private key

--- backend/api/createSafe.js
+++ backend/api/createSafe.js
@@ -9,3 +9,8 @@ const FIRST_OWNER_PK = process.env.PRIVATE_KEY
 if (!FIRST_OWNER_PK) {
   throw new Error('First owner private key not set in .env (PRIVATE_KEY)')
 }
+
+// Create New Private key.. later can be replaced by nillion?
+const owner2Wallet = Wallet.createRandom();
+console.log('owner2Wallet private key', owner2Wallet.privateKey)
+console.log('owner2Wallet address', owner2Wallet.address)
