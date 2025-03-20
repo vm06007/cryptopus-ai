5a838eb6d5a5dcfd43feaf411bbcb21479cb898e
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-03-21 00:13:45 +0700

    convert to Flask

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,10 +1,38 @@
-from http.server import BaseHTTPRequestHandler
+import os
+import certifi
+os.environ['SSL_CERT_FILE'] = certifi.where()
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask import request
 
-class handler(BaseHTTPRequestHandler):
+from web3 import Web3
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello ETHGlobal!'.encode('utf-8'))
-        return
+load_dotenv()
+
+app = Flask(__name__)
+
+CORS(app, resources={r"/*/*": {
+    "origins": [
+        "http://localhost:3000",
+        "http://localhost:4000",
+        "http://localhost:5000",
+    ]}
+})
+
+@app.route("/")
+def home():
+    return 'Hello, World!'
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    if request.method == 'POST':
+        return jsonify({"message": "hello"})
+    else:
+        # handle GET
+        return 'About'
+
+if __name__ == "__main__":
+    app.run()
+
+
