b7bee54f6fdfbc5e203356cabadb937c8aff5a58
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-03-20 19:29:09 -0300

    use rawArgs minmist

--- backend/api/proposeTransactionSafe.js
+++ backend/api/proposeTransactionSafe.js
@@ -2,3 +2,13 @@ require('dotenv').config();
 const minimist = require('minimist');
 const { ethers } = require('ethers');
 const axios = require('axios');
+
+const rawArgs = minimist(process.argv.slice(2), {
+    string: [
+      'safeAddress',
+      'destination',
+      'data',
+      'nonce',
+      'value'
+    ]
+  });
