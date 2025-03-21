b479a35b9da807b0b52583231843633bf1505aa3
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-03-21 10:35:30 -0300

    Create addAiAgentSigner.js

---
+++ backend/api/addAiAgentSigner.js
@@ -0,0 +1,9 @@
+const { ethers } = require('ethers')
+const SafeApiKit = require('@safe-global/api-kit')
+const Safe = require('@safe-global/protocol-kit')
+const { OperationType } = require('@safe-global/types-kit')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
