92d98c5f46368317b8c9a4f98899be6512b6b801
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-03-21 10:28:43 -0300

    safeAbiFragment

--- backend/api/proposeTransactionSafe.js
+++ backend/api/proposeTransactionSafe.js
@@ -80,3 +80,25 @@ async () => {
       process.exit(1);
     }
   }
+
+  const safeAbiFragment = [
+    {
+      inputs: [
+        { internalType: 'address', name: 'to',             type: 'address' },
+        { internalType: 'uint256', name: 'value',          type: 'uint256' },
+        { internalType: 'bytes',   name: 'data',           type: 'bytes' },
+        { internalType: 'uint8',   name: 'operation',      type: 'uint8' },
+        { internalType: 'uint256', name: 'safeTxGas',      type: 'uint256' },
+        { internalType: 'uint256', name: 'baseGas',        type: 'uint256' },
+        { internalType: 'uint256', name: 'gasPrice',       type: 'uint256' },
+        { internalType: 'address', name: 'gasToken',       type: 'address' },
+        { internalType: 'address', name: 'refundReceiver', type: 'address' },
+        { internalType: 'uint256', name: '_nonce',         type: 'uint256' }
+      ],
+      name: 'getTransactionHash',
+      outputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
+      stateMutability: 'view',
+      type: 'function'
+    }
+  ];
+
