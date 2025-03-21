52ed477cb9e90986c28b5b4e6edca69091a7756e
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-03-21 12:03:30 +0700

    format document (spaces)

--- frontend/wagmi-demo/components/SendTransaction.tsx
+++ frontend/wagmi-demo/components/SendTransaction.tsx
@@ -9,38 +9,38 @@ import type { SendTransactionVariables } from "wagmi/query";
 
 const SendTransaction = ({ to, amount, warning }: { to: string; amount: string, warning: string }) => {
 
-  if (!to) {
-    to = "0x641AD78BAca220C5BD28b51Ce8e0F495e85Fe689"
-  }
+    if (!to) {
+        to = "0x641AD78BAca220C5BD28b51Ce8e0F495e85Fe689"
+    }
 
-  if (!amount) {
-    amount = "0.01"
-  }
+    if (!amount) {
+        amount = "0.01"
+    }
 
-  const transactionRequest: SendTransactionVariables<Config, number> = {
-    to: to as `0x${string}`,
-    value: parseEther(amount),
-    type: "eip1559",
-  };
+    const transactionRequest: SendTransactionVariables<Config, number> = {
+        to: to as `0x${string}`,
+        value: parseEther(amount),
+        type: "eip1559",
+    };
 
-  const {data, isPending, isSuccess, sendTransaction} = useSendTransaction();
+    const { data, isPending, isSuccess, sendTransaction } = useSendTransaction();
 
-  return (
-    <Wrapper title="">
-      {warning && (
-        <div className="rounded bg-red-400 px-2 py-1 text-sm text-white">
-          We recommend doing this on sepolia.
-        </div>
-      )}
-      <Button
-        cta="Confirm Transaction"
-        onClick_={() => sendTransaction(transactionRequest)}
-        disabled={!sendTransaction}
-      />
-      {isPending && <div>Check wallet</div>}
-      {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
-    </Wrapper>
-  );
+    return (
+        <Wrapper title="">
+            {warning && (
+                <div className="rounded bg-red-400 px-2 py-1 text-sm text-white">
+                    We recommend doing this on sepolia.
+                </div>
+            )}
+            <Button
+                cta="Confirm Transaction"
+                onClick_={() => sendTransaction(transactionRequest)}
+                disabled={!sendTransaction}
+            />
+            {isPending && <div>Check wallet</div>}
+            {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+        </Wrapper>
+    );
 };
 
 export default SendTransaction;
