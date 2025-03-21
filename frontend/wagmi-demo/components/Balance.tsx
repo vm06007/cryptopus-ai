986135e39d06cda702a4bf01437f7f0b52a125a9
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-03-21 14:45:02 +0700

    cleanup balance display

--- frontend/wagmi-demo/components/Balance.tsx
+++ frontend/wagmi-demo/components/Balance.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import {useAccount, useBalance} from 'wagmi';
+import {useAccount, useBalance} from "wagmi";
 
 const Balance = () => {
   const {address} = useAccount();
@@ -10,13 +10,11 @@ const Balance = () => {
   if (isError) return <div>Error fetching balance</div>;
   return (
     <>
-      <h2 className="mt-6 text-2xl">useBalance</h2>
+      {/*<h2 className="mt-6 text-2xl">useBalance</h2>*/}
       {isLoading && <p>fetching balance...</p>}
       {isError && <p>Error fetching balance.</p>}
       {data && (
-        <p>
-          Balance: {data?.formatted} {data?.symbol}
-        </p>
+        <p>{data?.formatted} {data?.symbol}</p>
       )}
     </>
   );
